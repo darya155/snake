830ac6541a5e98a82eeadb3f3aec25762bbdf078
darya155 <fiola@mail.ru>
2017-07-12 18:32:57 +0300

    Абстрагирование. Создали класс Snake, Dirtction.

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -17,17 +17,46 @@ namespace Snake
             //Console.WriteLine("создается новая точка");
         }
 
-        public Point(int _x, int _y, char _sym)
+        public Point(int _x, int _y, char _sym)//конструктор
         {
             x = _x;
             y = _y;
             sym = _sym;
         }
-
+        public Point(Point p)//конструктор задает точки с помощью другой точки
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+        public void Move(int offset, Direction direction)//сдвигает точку на расстояние offset по направлению direction
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            if (direction == Direction.DOWN)
+            {
+                y = y - offset;
+            }
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.WriteLine(sym);
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
         }
     }
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,23 +10,9 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            //Инкапсуляция - свойство системы, позв. объединить данные и методы, раб. с ними, в классе.
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.DrawLine();
-
-            VerticalLine vline = new VerticalLine(10, 20, 4, '&');
-            vline.DrawLine();
-
-
-            //создали класс человек и работник
-            Emploee e = new Emploee();
-            e.age = 42;
-            e.pay = 30000;
+            Point p = new Point(4, 5, '#');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.DrawLine();
         }
     }
 }
